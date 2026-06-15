231f547584b0d54221311ef14c40e3b55fa312fe
codiy <mail@codiy.net>
2026-06-15 21:15:38 +0000

    fix

--- gcp/debian.sh
+++ gcp/debian.sh
@@ -48,3 +48,13 @@ sudo usermod -a -G docker codiy
 
 # MISC
 sudo apt install unzip
+
+# swap
+sudo fallocate -l 2G /swap
+sudo chmod 600 /swap
+sudo mkswap /swap
+sudo swapon /swap
+echo '/swap none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
